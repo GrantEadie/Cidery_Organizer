4a00454ce4de6f15e73bbf59d8ae4fd362c7ac5f
Grant Eadie <grantleadie@gmail.com>
2020-10-28 11:49:39 -0700

    update register styling.

--- CideryOrganizer/Controllers/HomeController.cs
+++ CideryOrganizer/Controllers/HomeController.cs
@@ -1,13 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
+using CideryOrganizer.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace CideryOrganizer.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly CideryOrganizerContext _db;
+
+    public HomeController(CideryOrganizerContext db)
+    {
+      _db = db;
+    }
 
     [HttpGet("/")]
     public ActionResult Index()
     {
+      var thisCider = _db.Ciders.FirstOrDefault();
+      ViewBag.AppleId = new SelectList(_db.Apples, "AppleId", "AppleName");
       return View();
     }
   }
